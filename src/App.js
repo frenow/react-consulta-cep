778d4f0752fa1477e66ff7972e6378d8d9260484
Emerson <emerson2@softrom.local>
2020-06-26 17:34:15 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Downshift from "downshift";
+import axios from "axios";
 
-function App() {
+const App = () => {
+
+  const[endereco, setEndereco] = useState([]);
+  const[logradouro, setLogradouro] = useState();
+  
+
+  function inputOnChange(event) {
+    if (!event.target.value) {
+      return;
+    }
+    fetchMovies(event.target.value);
+  }
+
+  function downshiftOnChange(selectedMovie) {
+    alert(`your favourite movie is ${selectedMovie.title}`);
+  }  
+
+  function fetchMovies(rua) {
+    const viacepURL = `viacep.com.br/ws/MG/Belo Horizonte/${rua}/json/`;
+    axios.get(viacepURL).then(response => {
+      setEndereco(response.data);
+      
+      console.log(endereco);
+    });
+  }  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Downshift
+        onChange={downshiftOnChange}
+        itemToString={item => (item ? logradouro : "")}
+      >
+        {({
+          selectedItem,
+          getInputProps,
+          getItemProps,
+          highlightedIndex,
+          isOpen,
+          inputValue,
+          getLabelProps
+        }) => (
+          <div>
+            <label
+              style={{ marginTop: "1rem", display: "block" }}
+              {...getLabelProps()}
+            >
+              Pesquisa de Endereço
+            </label>{" "}
+            <br />
+            <input
+              {...getInputProps({
+                placeholder: "Digite o endereço",
+                onChange: inputOnChange
+              })}
+            />
+            {isOpen ? (
+              <div className="downshift-dropdown">
+                {endereco.logradouro
+                  .filter(
+                    item =>
+                      !inputValue ||
+                      item.logradouro
+                        .toLowerCase()
+                        .includes(inputValue.toLowerCase())
+                  )
+                  .slice(0, 10)
+                  .map((item, index) => (
+                    <div
+                      className="dropdown-item"
+                      {...getItemProps({ key: index, index, item })}
+                      style={{
+                        backgroundColor:
+                          highlightedIndex === index ? "lightgray" : "white",
+                        fontWeight: selectedItem === item ? "bold" : "normal"
+                      }}
+                    >
+                      {item.logradouro}
+                    </div>
+                  ))}
+              </div>
+            ) : null}
+          </div>
+        )}
+      </Downshift>
+
     </div>
   );
 }
